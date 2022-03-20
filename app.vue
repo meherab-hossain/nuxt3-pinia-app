d4d8ff97e92de9088a4e6a041ff0e79779293e45
meherab-hossain <h.meherabhossain@gmail.com>
2022-03-20 21:45:50 +0600

    init

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <NuxtPage>
+    Hello world!
+  </NuxtPage>
+</template>
---
+++ pages/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <NuxtLayout name="custom">
+     hello
+  </NuxtLayout>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
